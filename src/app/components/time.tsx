902d96490986bb2ae30e58d67402b400c9dd58a9
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-03-18 01:03:46 +0530

    timer made

---
+++ src/app/components/time.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { useEffect, useRef, useState } from "react";
+
+export default function Time() {
+
+    let time = new Date().toLocaleTimeString()
+    const timeRef = useRef(time)
+    const [ctime, setTime] = useState(timeRef.current);
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            timeRef.current = new Date().toLocaleTimeString();
+            setTime(timeRef.current)
+        }, 1000);
+
+        return () => clearInterval(interval); 
+    }, []);
+    return(
+        <div >{ctime}</div>
+    )
+}
